490bec1458c863cb49f2e086fbc5c68393e724e5
Sofiane Mouhcine <sofiane@dokusai.com>
2025-12-27 23:45:31 +0000

    Fix display email domain: contact@provenai.org (was .io)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 // Email constants
 // Display email shown on website
-export const DISPLAY_EMAIL = 'contact@provenai.io'
+export const DISPLAY_EMAIL = 'contact@provenai.org'
 // Actual email address for mailto links and email delivery
 export const CONTACT_EMAIL = 'sofiane@provenai.org'
 
